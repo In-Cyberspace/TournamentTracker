6fa083813f9163762d4e7c339796c9ed89f3edb9
Hugo Molaudzi <hibikeportal@Cybers-MBP.Dlink>
2019-11-20 13:00:32 +0200

    Made updates to the class library (TrackerLibray)

---
+++ TrackerLibrary/MatchupModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace TrackerLibrary
+{
+    public class MatchupModel
+    {
+        public List<MatchupEntryModel> Entries { get; set; } =
+            new List<MatchupEntryModel>;
+    }
+}
